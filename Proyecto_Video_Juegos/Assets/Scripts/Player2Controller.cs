68ba3de074efb69987782442ad97f363aa81c194
Gustavo de Leon <gdeleontobias@gmail.com>
2019-05-10 13:21:03 -0600

    avances de movimiento

--- Proyecto_Video_Juegos/Assets/Scripts/Player2Controller.cs
+++ Proyecto_Video_Juegos/Assets/Scripts/Player2Controller.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player1Controller : MonoBehaviour {
+public class Player2Controller : MonoBehaviour  {
 
 	public float moveSpeed;
 	public float jumpForce;
@@ -16,18 +16,18 @@ public class Player1Controller : MonoBehaviour {
 
 		controller = GetComponent<CharacterController> ();
 		//theRB = GetComponent<Rigidbody>();
-		
+
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 
-		moveDirection = new Vector3 (Input.GetAxis ("Horizontal") * moveSpeed, 0f, Input.GetAxis ("Vertical") * moveSpeed);
+		moveDirection = new Vector3 (Input.GetAxis ("Horizontal2") * moveSpeed, 0f, Input.GetAxis ("Vertical2") * moveSpeed);
 		//theRB.velocity = new Vector3(Input.GetAxis ("Horizontal") * moveSpeed, theRB.velocity.y, Input.GetAxis ("Vertical") * moveSpeed);
 
 		if (Input.GetButtonDown ("Jump")) 
 		{
-			
+
 			moveDirection.y = jumpForce;
 			//theRB.velocity = new Vector3(theRB.velocity.x, jumpForce, theRB.velocity.z);
 		}
